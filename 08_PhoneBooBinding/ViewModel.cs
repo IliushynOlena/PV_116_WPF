bc5770cff39fdd2fab73a0f91708075319b9227f
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2022-12-14 20:44:40 +0200

    Bindings

---
+++ 08_PhoneBooBinding/ViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace _08_PhoneBooBinding
+{
+    public class ViewModel
+    {
+        ObservableCollection<Contact> contacts = new ObservableCollection<Contact>();
+
+        public IEnumerable<Contact> Contacts => contacts;
+        public Contact SelectedContact { get; set; }
+
+        public ViewModel()
+        {
+            contacts.Add(new Contact() { Name = "Vova", Age = 30, Surname = "Pupkin", Phone = "+3807575828", IsMale = true });
+            contacts.Add(new Contact() { Name = "Marusia", Age = 25, Surname = "Shishik", Phone = "+380771244", IsMale = false });
+            contacts.Add(new Contact() { Name = "Olga", Age = 33, Surname = "Shelesh", Phone = "+38067285792", IsMale = false });
+
+        }
+        public void DeleteteSelectionContact()
+        {
+            if(SelectedContact != null)
+                contacts.Remove(SelectedContact);
+        }
+    }
+}
